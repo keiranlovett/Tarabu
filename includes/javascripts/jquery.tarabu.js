2c030236a135c8a8a973494ef949ab4cef3c0134
keiranlovett <kvclovett@gmail.com>
2012-09-29 01:17:22 +0800

    json parse changes

--- includes/javascripts/jquery.tarabu.js
+++ includes/javascripts/jquery.tarabu.js
@@ -1,54 +1,39 @@
-/*soundManager.setup({
-  url: '/path/to/swf-files/',
-  onready: function() {
-    var mySound = soundManager.createSound({
-      id: 'aSound',
-      url: '/path/to/an.mp3'
-    });
-    mySound.play();
-  },
-  ontimeout: function() {
-    // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
-  }
-});
-
-*/
-
-
 (function($) {
 
 	$.fn.Tarabu = function(options) {
 		$.fn.Tarabu.defaults = {
-			soundcloud_user: null,
+			soundcloud_URL: null,
 			soundcloud_clientId: null,
 			playlist: null,
+			page_title: document.title,
 			locked: null
 		};
 		var o = $.extend({}, $.fn.Tarabu.defaults, options);
-		return this.each(function() {			
-			//Determine playlist type. If soundcloud is set as the playlist it will load either the complete user set (soundcloud_user must be set) or selected sets. If local playlists have been set it will load them.
+		
+		return this.each(function() {	
+			var c = $(this);		
+			//Determine playlist type. If soundcloud is set as the playlist it will load either the complete user set (soundcloud_URL must be set) or selected sets. If local playlists have been set it will load them.
 			if (o.playlist == 'soundcloud') {
-				alert('soundcloud');
 				//Determine if the soundcloud API client ID is set. This is required to access json data.
 				if(typeof(o.soundcloud_clientId) != "undefined" && o.soundcloud_clientId !== null) {
 					//Determine if the user is set and then load all sets.
-					if(typeof(o.soundcloud_user) != "undefined" && o.soundcloud_user !== null) {
-						alert('soundcloud user set');	
-						$.getJSON('http://api.soundcloud.com/users/'+ o.soundcloud_user +'/playlists.json?client_id=' + o.soundcloud_clientId, { get_param: 'value' },
+					if(typeof(o.soundcloud_URL) != "undefined" && o.soundcloud_URL !== null) {
+					/*	var patt = new RegExp('users', 'g'); 
+						if(patt.test(URLString) == true) { //secondary level
+							}else{
+							//primary level 
+							}*/
+						$.getJSON('http://api.soundcloud.com/'+ o.soundcloud_URL +'?client_id=' + o.soundcloud_clientId, { get_param: 'value' },
 						function(playlists_data) {
 							$.each(playlists_data, function(index, playlists) {
 							//Get playlists data
 							i = 1;
 							$.getJSON(playlists.uri + '.json?client_id=' + o.soundcloud_clientId, { get_param: 'value' }, function(playlist) {
-								$("<div class='player row' id='id" + i +"'><div class=' four columns'><div class='artwork'><img /></div></div><div class='playlist eight columns'><div class='description'></div><ol class='tracks'></ol></div></div>").appendTo('.soundcloud');
+								c.append("<div class='tarabu-playlist' id='tarabu-id" + i + "'><div class='tarabu-sleeve'><img class='tarabu-artwork'/></div><div class='tarabu-content'><div class='tarabu-description'></div><ol class='tarabu-tracks'></ol></div></div>");
 								//get track data
 								$.each(playlist.tracks, function(index, track) {
 									// Create a list item for track, associate it's data, and append it to the track list.
-									var $li = $('<li class="track_' + track.id + '">' + track.title + '</li>').data('track', track).appendTo('#id'+i+' .tracks');
-								// Find the appropriate stream url depending on whether the track has a secret_token or is public.
-									url = track.stream_url;
-									(url.indexOf("secret_token") == -1) ? url = url + '?' : url = url + '&';
-									url = url + 'client_id=' + o.client_id;
+									var $li = $('<li class="tarabu-track-' + track.id + '">' + track.title + '</li>').data('track', track).appendTo('#tarabu-id'+i+' .tarabu-tracks');
 								});
 								i++			
 							});
